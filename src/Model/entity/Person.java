d6c4a0d2d81d56ff0b6d23cafba5bae02d5fc35c
LENOVO <nazaninzahra.heydari2004@gmail.com>
2024-10-03 20:24:13 +0330

    Nazanin added

--- src/Model/entity/Person.java
+++ src/Model/entity/Person.java
@@ -19,8 +19,8 @@ public class Person {
     private String nationalID;
     private String phoneNumber;
     private Role role;
-    private boolean status; //validation??
+    private boolean status; //todo:validation??
     private String userName;
     private String password;
-    private String accessLevel; //validation??
+    private String accessLevel; //todo:validation??
 }
